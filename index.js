a97e429c96a22dfb348ad59b51aaa3a5a6c98bac
Ebla Ekerdiker <eblaekerdiker@gmail.com>
2023-05-07 17:49:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun('bathe my dog');
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(wrapper = '||') {
+    return function inner(adjective="special"){
+        return `You are ${wrapper}${adjective}${wrapper}!`
+    }
+}
+
+wrapAdjective("a dedicated programmer");
